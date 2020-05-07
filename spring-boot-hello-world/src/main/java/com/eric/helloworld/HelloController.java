fa96df6ec08dd566a10d48e8a5ff03ab2a532b40
钱亮 <120444121@hdjt.hdad.local>
2020-05-07 13:03:58 +0800

    hello world提交

--- spring-boot-hello-world/src/main/java/com/eric/helloworld/HelloController.java
+++ spring-boot-hello-world/src/main/java/com/eric/helloworld/HelloController.java
@@ -2,11 +2,13 @@ package com.eric.helloworld;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Eric
  * @create 2018 04 12 21:39
  */
+@RestController
 public class HelloController {
     @RequestMapping("/hello")
     @ResponseBody
--- spring-boot-hello-world/src/main/java/com/eric/helloworld/HelloworldApplication.java
+++ spring-boot-hello-world/src/main/java/com/eric/helloworld/HelloworldApplication.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @Slf4j
 public class HelloworldApplication {
 
